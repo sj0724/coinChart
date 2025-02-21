358d623f566da512af69f5597aa56206125a2626
sj0724 <sj07245@naver.com>
2025-02-21 10:05:59 +0900

    feat: stmbol 정보 컴포넌트 구현

---
+++ app/trade/[name]/_components/symbolInfo.tsx
@@ -0,0 +1,39 @@
+import { SymbolData } from '@/types/binance';
+import { formatNumber } from '@/utils/formatNumber';
+
+interface Props {
+  name: string;
+}
+
+export default async function SymbolInfo({ name }: Props) {
+  const data = await fetch(`http://localhost:3000/api/detail?name=${name}`);
+  const result: SymbolData = await data.json();
+  return (
+    <div className='h-16 p-4 flex items-center justify-between gap-3 border rounded-md max-w-[750px]'>
+      <div className='flex gap-2'>
+        <p className='text-xl font-semibold'>{name}</p>
+        <p className='text-xl font-semibold'>
+          {formatNumber(result.lastPrice)}
+        </p>
+      </div>
+      <div className='flex gap-4'>
+        <div className='flex flex-col text-xs'>
+          <p className='text-gray-500'>24h High</p>
+          <p>{formatNumber(result.highPrice)}</p>
+        </div>
+        <div className='flex flex-col text-xs'>
+          <p className='text-gray-500'>24h Low</p>
+          <p>{formatNumber(result.lowPrice)}</p>
+        </div>
+        <div className='flex flex-col text-xs'>
+          <p className='text-gray-500'>24h Volume - count</p>
+          <p>{formatNumber(result.volume)}</p>
+        </div>
+        <div className='flex flex-col text-xs'>
+          <p className='text-gray-500'>24h Volume - currency</p>
+          <p>{formatNumber(result.quoteVolume)}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
