4ab239b78a8db307f528d56965f340f10801caf5
sj0724 <sj07245@naver.com>
2025-02-21 23:45:45 +0900

    feat: 마우스 호버시 상세 데이터 표시

--- app/trade/[name]/_components/chart.tsx
+++ app/trade/[name]/_components/chart.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { createChart, CandlestickSeries } from 'lightweight-charts';
+import { Candle } from '@/types/chart';
 
 interface Props {
   symbol: string;
@@ -9,6 +10,7 @@ interface Props {
 
 export default function Chart({ symbol }: Props) {
   const chartRef = useRef<HTMLDivElement>(null);
+  const [hoverData, setHoverData] = useState<Candle | null>(null);
 
   useEffect(() => {
     if (!chartRef.current) return;
@@ -26,7 +28,7 @@ export default function Chart({ symbol }: Props) {
       );
       const result = await data.json();
 
-      const formattedData = result.map((candle: any) => ({
+      const formattedData: Candle[] = result.map((candle: any) => ({
         time: candle[0] / 1000, // UNIX timestamp (초 단위)
         open: parseFloat(candle[1]),
         high: parseFloat(candle[2]),
@@ -37,6 +39,13 @@ export default function Chart({ symbol }: Props) {
       candleSeries.setData(formattedData);
     };
 
+    chart.subscribeCrosshairMove((param) => {
+      const data = param.seriesData.get(candleSeries);
+      if (data) {
+        setHoverData(data as Candle);
+      }
+    });
+
     fetchCandles();
 
     return () => {
@@ -44,5 +53,28 @@ export default function Chart({ symbol }: Props) {
     };
   }, []);
 
-  return <div ref={chartRef} className='border rounded-md p-5' />;
+  return (
+    <div className='relative'>
+      {hoverData && (
+        <div className='absolute top-0 w-full p-4 rounded-md z-30 flex gap-5 text-sm'>
+          <p>{new Date(Number(hoverData.time) * 1000).toLocaleDateString()}</p>
+          {Object.keys(hoverData)
+            .filter((key) => key !== 'time')
+            .map((key) => {
+              const value = hoverData[key as keyof Candle];
+              const textColor =
+                hoverData.open > hoverData.close
+                  ? 'text-red-600'
+                  : 'text-green-500';
+              return (
+                <p key={key}>
+                  {key} : <span className={textColor}>{value.toString()}</span>
+                </p>
+              );
+            })}
+        </div>
+      )}
+      <div ref={chartRef} className='border rounded-md p-5' />
+    </div>
+  );
 }
---
+++ types/chart.ts
@@ -0,0 +1,9 @@
+import { Time } from 'lightweight-charts';
+
+export type Candle = {
+  time: Time;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+};
