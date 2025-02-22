6a61b550b268d0f284c5980cff327fec9c19725c
sj0724 <sj07245@naver.com>
2025-02-22 22:01:01 +0900

    fix: endTime 여부에 따라 searchParams 생성 기능 추가

--- app/api/chart/route.ts
+++ app/api/chart/route.ts
@@ -2,13 +2,20 @@ export async function GET(req: Request) {
   try {
     const url = new URL(req.url);
     const symbolName = url.searchParams.get('name') || '';
+    const limit = url.searchParams.get('limit') || '200';
+    const endTime = url.searchParams.get('endTime');
 
-    // 24시간 거래량 데이터
-    const response = await fetch(
-      `https://api.binance.com/api/v3/klines?symbol=${symbolName}&interval=1w&limit=500`
-    );
+    const apiUrl = new URL('https://api.binance.com/api/v3/klines');
+    apiUrl.searchParams.append('symbol', symbolName);
+    apiUrl.searchParams.append('interval', '1w');
+    apiUrl.searchParams.append('limit', limit);
+
+    if (endTime) {
+      apiUrl.searchParams.append('endTime', endTime);
+    }
+
+    const response = await fetch(apiUrl.toString());
     const data = await response.json();
-
     return Response.json(data, { status: 200 });
   } catch (error) {
     return Response.json(
