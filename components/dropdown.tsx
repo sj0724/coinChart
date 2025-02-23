f9142abc26dc44f583d84a62a6b3e41ed0a3f555
sj0724 <sj07245@naver.com>
2025-02-23 22:19:56 +0900

    feat: 드롭다운 구현

---
+++ components/dropdown.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+
+type DropdownItme = {
+  name: string;
+  value: string;
+};
+
+interface Props {
+  onClick: (value: string) => void;
+  list: DropdownItme[];
+  value: string;
+}
+
+export default function Dropdown({ list, value, onClick }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className='relative' ref={dropdownRef}>
+      <button type='button' onClick={() => setIsOpen(!isOpen)}>
+        {list.find((item) => item.value === value)?.name}
+      </button>
+      {isOpen && (
+        <ul className='flex flex-col p-3 gap-1 absolute right-0 w-28 bg-white items-center shadow-md rounded-md'>
+          {list.map((item, index) => (
+            <li key={index} className='w-full'>
+              <button
+                type='button'
+                className='p-3 w-full hover:bg-gray-100 rounded-md text-nowrap'
+                onClick={() => onClick(item.value)}
+              >
+                {item.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
