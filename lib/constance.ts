f87aa4acaccfca385b63d98a5ad2dcc6f5dd8210
sj0724 <sj07245@naver.com>
2025-02-24 00:32:58 +0900

    fix: 상수값 분리

---
+++ lib/constance.ts
@@ -0,0 +1,11 @@
+export const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/api'
+    : `${process.env.NEXT_PUBLIC_APP_URL}/api`;
+
+export const BASE_WS_URL = 'wss://stream.binance.com:9443/ws';
+
+export const BASE_BINANCE_URL = 'https://api.binance.com/api/v3';
+
+export const DEFAULT_STALE_TIME = 3 * 60 * 1000; // 캐싱 유지 3분
+export const LIMIT = 200; // 데이터 갯수
