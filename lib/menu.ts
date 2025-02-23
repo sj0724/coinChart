3605413e80118efbd529d8dcefcd7e9cfbbb0b89
sj0724 <sj07245@naver.com>
2025-02-24 00:33:05 +0900

    fix: 메뉴 값 분리

---
+++ lib/menu.ts
@@ -0,0 +1,7 @@
+export const SORT_MENU = [
+  { name: '가격내림차순', value: 'priceDes' },
+  { name: '가격오름차순', value: 'priceAsc' },
+  { name: '거래량순', value: 'volume' },
+];
+
+export const INTERVAL_MENU = ['3m', '15m', '1h', '4h', '1d', '1w'];
