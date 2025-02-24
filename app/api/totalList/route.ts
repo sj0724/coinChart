f60869caacbaea6bb9965cac6a6b77b38d58ac83
sj0724 <sj07245@naver.com>
2025-02-24 15:45:48 +0900

    fix: 거래중인 자산 조회 후 해당 자산으로 검색, batch 적용

--- app/api/totalList/route.ts
+++ app/api/totalList/route.ts
@@ -1,5 +1,5 @@
 import { BASE_BINANCE_URL } from '@/lib/constance';
-import { SymbolData } from '@/types/binance';
+import { ExchangeInfo, SymbolData } from '@/types/binance';
 
 export async function GET(req: Request) {
   try {
@@ -7,16 +7,46 @@ export async function GET(req: Request) {
     const sortBy = url.searchParams.get('sortBy') || 'volume'; // 기본값 volume(거래량)
     const currencyFilter = url.searchParams.get('currency') || ''; // 특정 통화 필터링, 기본값 전체
 
-    // 24시간 거래량 데이터
-    const response = await fetch(`${BASE_BINANCE_URL}/ticker/24hr`);
-    const data = await response.json();
+    // 거래중인 자산 조회
+    const request = await fetch(
+      `${BASE_BINANCE_URL}/exchangeInfo?symbolStatus=TRADING`
+    );
+    const totalExchange: ExchangeInfo = await request.json();
 
-    // 특정 통화 필터링
-    const filteredData: SymbolData[] = data.filter((item: SymbolData) =>
-      currencyFilter ? item.symbol.includes(currencyFilter) : true
+    // currency 필터링
+    const currencyPairs = totalExchange.symbols
+      .filter((s) => s.quoteAsset === currencyFilter)
+      .map((s) => s.symbol);
+
+    const batches = []; // symbols 요청을 나눠서 보내기 위한 batch 배열
+
+    for (let i = 0; i < currencyPairs.length; i += 100) {
+      batches.push(currencyPairs.slice(i, i + 100));
+    }
+
+    const results: SymbolData[] = []; // 분할 실행한 결과를 담을 배열
+
+    await Promise.all(
+      batches.map(async (item) => {
+        try {
+          const symbolsList = JSON.stringify(item);
+          const response = await fetch(
+            `${BASE_BINANCE_URL}/ticker/24hr?symbols=${symbolsList}`
+          );
+
+          if (!response.ok) {
+            throw new Error('URL 변환 중 오류가 발생했습니다.');
+          }
+
+          const res = await response.json();
+          results.push(...res);
+        } catch (err) {
+          console.error('URL 변환 중 오류가 발생했습니다.');
+        }
+      })
     );
 
-    const sortedData = filteredData.sort((a, b) => {
+    const sortedData = results.sort((a, b) => {
       if (sortBy === 'priceDes') {
         // 가격 내림차순 정렬
         return parseFloat(b.lastPrice) - parseFloat(a.lastPrice);
