c51f9ab31f8e1478cc0b63c330a6f56efa101707
sj0724 <sj07245@naver.com>
2025-03-10 16:49:11 +0900

    fix: 서버 컴포넌트에서 클라이언트 컴포넌트로 변경

--- app/en/trade/[name]/page.tsx
+++ app/en/trade/[name]/page.tsx
@@ -1,16 +1,26 @@
+'use client';
+
 import SymbolInfo from './_components/symbolInfo';
 import OrderBookContainer from './_components/orderContainer';
 import Chart from './_components/chart';
 import TradingList from './_components/tradingList';
 import SymbolList from './_components/symbolList';
 import TradingBoard from './_components/tradingBoard';
+import { useWebSocketStore } from '@/store/useWebsocketStore';
+import { use, useEffect } from 'react';
 
-export default async function Page({
+export default function Page({
   params,
 }: {
   params: Promise<{ name: string }>;
 }) {
-  const symbolName = (await params).name;
+  const { name: symbolName } = use(params);
+  const setSymbol = useWebSocketStore((state) => state.setSymbol);
+
+  useEffect(() => {
+    setSymbol(symbolName);
+  }, [symbolName]);
+
   return (
     <div className='flex w-screen gap-2 justify-center py-2'>
       <OrderBookContainer symbol={symbolName} />
