9ea0ca85bf77db1566fb73a1eab8f9c0848b0b51
sj0724 <sj07245@naver.com>
2025-03-13 17:02:32 +0900

    feat: depth 초기 데이터 패칭 로직 구현

--- app/api/order/helper.ts
+++ app/api/order/helper.ts
@@ -1,1 +1,25 @@
-console.log('test');
+import { BASE_BINANCE_URL } from '@/lib/constance';
+import { Order } from '@/types/binance';
+
+export const fetchDepthList = async (symbol: string) => {
+  try {
+    const request = await fetch(
+      `${BASE_BINANCE_URL}/depth?symbol=${symbol}&limit=1000`
+    );
+    const depthList: Order = await request.json();
+
+    const askList = Array.isArray(depthList?.asks)
+      ? [...depthList.asks]
+          .filter((ask) => ask[1] !== '0.00000000')
+          .slice(0, 20)
+          .reverse()
+      : [];
+    const bidList = Array.isArray(depthList?.bids)
+      ? depthList?.bids.filter((bids) => bids[1] !== '0.00000000').slice(0, 20)
+      : [];
+
+    return { asks: askList ?? [], bids: bidList ?? [] };
+  } catch (error) {
+    console.error('Error fetching order book:', error);
+  }
+};
