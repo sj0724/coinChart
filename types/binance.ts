ccaa1b041a7c9533b41a9144765d3a65695fff64
sj0724 <sj07245@naver.com>
2025-03-16 17:31:49 +0900

    fix: 타입 수정

--- types/binance.ts
+++ types/binance.ts
@@ -1,18 +1,9 @@
 export type SymbolData = {
   symbol: string;
-  priceChange: string;
-  priceChangePercent: string;
-  weightedAvgPrice: string;
-  prevClosePrice: string;
-  lastPrice: string;
-  lastQty: string;
-  bidPrice: string;
-  bidQty: string;
-  askPrice: string;
-  askQty: string;
   openPrice: string;
   highPrice: string;
   lowPrice: string;
+  lastPrice: string;
   volume: string;
   quoteVolume: string;
   openTime: number;
