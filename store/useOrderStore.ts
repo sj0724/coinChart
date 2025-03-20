01135e95ffe0ee854af3bd46699c80984bd7ce22
sj0724 <sj07245@naver.com>
2025-03-20 16:36:05 +0900

    fix: 웹소켓 depth 데이터 변경시마다 주문 확인

--- store/useOrderStore.ts
+++ store/useOrderStore.ts
@@ -1,4 +1,6 @@
 import { create } from 'zustand';
+import { useWebSocketStore } from './useWebsocketStore';
+import { orderCalculator } from '@/utils/orderCalculator';
 
 export type OrderType = 'ask' | 'bid';
 
@@ -11,18 +13,55 @@ interface State {
   ask: Order;
   bid: Order;
   setOrder: (type: OrderType, order: Order) => void;
+  checkOrderMatch: () => void;
 }
 
-const useOrderStore = create<State>((set) => ({
+const useOrderStore = create<State>((set, get) => ({
   ask: { price: 0, amount: 0 },
   bid: { price: 0, amount: 0 },
   setOrder: (type, order) => {
     if (type === 'ask') {
       set({ ask: order });
-    } else if (type === 'bid') {
+    } else {
       set({ bid: order });
     }
   },
+  checkOrderMatch: () => {
+    const { ask, bid } = get();
+    const { depthUpdate } = useWebSocketStore.getState();
+    const { symbol } = useWebSocketStore.getState();
+    if (!depthUpdate) return;
+
+    const matchedAsk = depthUpdate.asks.find(
+      (order) => Number(order[0]) === ask.price
+    );
+    const matchedBid = depthUpdate.bids.find(
+      (order) => Number(order[0]) === bid.price
+    );
+
+    if (matchedAsk) {
+      const result = {
+        price: ask.price,
+        amount:
+          ask.amount >= Number(matchedAsk[1])
+            ? Number(matchedAsk[1])
+            : Number(matchedAsk[1]) - ask.amount,
+      };
+      orderCalculator('ask', result, symbol);
+      console.log('매도 주문 체결됨:', result);
+      set({ ask: { price: 0, amount: 0 } });
+    }
+
+    if (matchedBid) {
+      console.log('매수 주문 체결됨:', matchedBid);
+      set({ bid: { price: 0, amount: 0 } });
+    }
+  },
 }));
+
+// WebSocket Store 구독하여 depthUpdate 변경 감지
+useWebSocketStore.subscribe(() => {
+  useOrderStore.getState().checkOrderMatch();
+});
 
 export default useOrderStore;
