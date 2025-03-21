e0768d43c883dedac5118bb2319ab3d4f477c321
sj0724 <sj07245@naver.com>
2025-03-21 20:15:15 +0900

    feat: 카카오 로그인 로직 구현

---
+++ app/api/auth/helper.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { createClientForServer } from '@/utils/supabase/server';
+import { Provider } from '@supabase/supabase-js';
+import { redirect } from 'next/navigation';
+
+const signInWith = (provider: Provider) => async () => {
+  const supabase = await createClientForServer();
+  const auth_callback_url = `${process.env.SITE_URL}/auth/callback`;
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider,
+    options: {
+      redirectTo: auth_callback_url,
+    },
+  });
+  if (error) {
+    console.log(error);
+  }
+
+  redirect(data.url as string);
+};
+
+const signOut = async () => {
+  const supabase = await createClientForServer();
+  await supabase.auth.signOut();
+};
+
+const signInWithKakao = signInWith('kakao');
+
+export { signInWithKakao, signOut };
