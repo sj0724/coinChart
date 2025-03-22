9fe920b22df286ce590435395dc7d64d70c6bd20
sj0724 <sj07245@naver.com>
2025-03-22 18:47:21 +0900

    fix: 도메인 설정 추가

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -37,7 +37,15 @@ export async function GET(request: NextRequest) {
     }
 
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`);
+      const forwardedHost = request.headers.get('x-forwarded-host');
+      const isLocalEnv = process.env.NODE_ENV === 'development';
+      if (isLocalEnv) {
+        return NextResponse.redirect(`${origin}${next}`);
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
     }
   }
 
