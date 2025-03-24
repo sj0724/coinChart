03834a6c1fef8468bfd610f5eda05bd5a50189c2
sj0724 <sj07245@naver.com>
2025-03-24 11:56:21 +0900

    feat: 토스트 provider적용 및 주문 페이지 구현

--- app/(protected)/order-list/page.tsx
+++ app/(protected)/order-list/page.tsx
@@ -1,3 +1,48 @@
+'use client';
+
+import { getOrder } from '@/app/api/order/helper';
+import { DbOrder } from '@/types/dbData';
+import { useEffect, useState } from 'react';
+
 export default function Page() {
-  return <div>주문 리스트</div>;
+  const [askOrderList, setAskOrderList] = useState<DbOrder[]>([]);
+  const [bidOrderList, setBidOrderList] = useState<DbOrder[]>([]);
+
+  useEffect(() => {
+    const fetctOrder = async () => {
+      const result = await getOrder();
+      if (result) {
+        const askArray = result.filter((item) => item.type === 'ASK');
+        const bidArray = result.filter((item) => item.type === 'BID');
+        setAskOrderList(askArray);
+        setBidOrderList(bidArray);
+      }
+    };
+    fetctOrder();
+  }, []);
+
+  return (
+    <div>
+      <p>매도 주문</p>
+      <ul>
+        {askOrderList.map((item) => (
+          <li key={item.id} className='flex gap-2'>
+            <p>{item.symbol}</p>
+            <p>{item.price}$</p>
+            <p>{item.amount}</p>
+          </li>
+        ))}
+      </ul>
+      <p>매수 주문</p>
+      <ul>
+        {bidOrderList.map((item) => (
+          <li key={item.id} className='flex gap-2'>
+            <p>{item.symbol}</p>
+            <p>{item.price}$</p>
+            <p>{item.amount}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
