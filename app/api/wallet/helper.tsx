6ba36e29482f66ef08aeb1e0620fd2056f7ccf87
sj0724 <sj07245@naver.com>
2025-03-27 21:54:15 +0900

    fix: 지갑 업데이트 로직 수정

--- app/api/wallet/helper.tsx
+++ app/api/wallet/helper.tsx
@@ -22,16 +22,15 @@ export const updateWallet = async (order: DbOrder) => {
     .from('wallet')
     .select('*')
     .eq('user_id', user.id)
-    .eq('symbol', order.symbol)
-    .single();
-  if (!wallet.data) {
+    .eq('symbol', order.symbol);
+  if (wallet.data?.length === 0) {
     // 지갑에 자산 없는 경우 새로 생성
     await supabase
       .from('wallet')
       .insert({ user_id: user.id, symbol: order.symbol, amount: order.amount });
   } else {
     // 이미 자산이 있는 경우 거래 타입에 따라 갯수 업데이트(매수: 추가)
-    const symbolWallet = wallet.data;
+    const symbolWallet = wallet.data![0];
     const newAmount = symbolWallet.amount + order.amount;
     await supabase
       .from('wallet')
