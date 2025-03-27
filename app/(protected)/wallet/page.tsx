e3e2aa8dd8383706fd4647d239d321ae9057526c
sj0724 <sj07245@naver.com>
2025-03-27 21:54:01 +0900

    feat: 지갑 페이지 구현

--- app/(protected)/wallet/page.tsx
+++ app/(protected)/wallet/page.tsx
@@ -1,32 +1,38 @@
-'use client';
-
 import { getWallet } from '@/app/api/wallet/helper';
-import { DbWallet } from '@/types/dbData';
-import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
-export default function Page() {
-  const [walletData, setWalletData] = useState<DbWallet[]>([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await getWallet();
-      if (result) {
-        setWalletData(result);
-      }
-    };
-    fetchData();
-  }, []);
+export default async function Page() {
+  const data = await getWallet();
+
+  if (!data || data.length === 0)
+    return (
+      <div className='flex gap-2 h-[calc(100vh-64px)] p-2 w-full justify-center'>
+        <div className='flex flex-col bg-white p-2 rounded max-w-[400px] w-full'>
+          <p className='font-bold text-lg p-2 border-b mb-2'>지갑</p>
+          <div className='flex flex-col items-center justify-center m-3 rounded h-full bg-gray-100 font-semibold'>
+            소유한 자산이 없습니다.
+          </div>
+        </div>
+      </div>
+    );
 
   return (
-    <div>
-      지갑
-      <ul className='flex flex-col'>
-        {walletData.map((item) => (
-          <li key={item.id} className='flex gap-2'>
-            <p>{item.symbol}</p>
-            <p>{item.amount}</p>
-          </li>
-        ))}
-      </ul>
+    <div className='flex gap-2 h-[calc(100vh-64px)] p-2 w-full justify-center'>
+      <div className='flex flex-col bg-white p-2 rounded max-w-[400px] w-full'>
+        <p className='font-bold text-lg p-2 border-b mb-2'>지갑</p>
+        <ul className='flex flex-col gap-2'>
+          {data.map((item) => (
+            <li key={item.id}>
+              <Link href={`/trade/${item.symbol}`}>
+                <div className='flex p-2 justify-between shadow rounded hover:bg-gray-100 text-sm'>
+                  <p className='font-bold'>{item.symbol}</p>
+                  <p>{item.amount}</p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
