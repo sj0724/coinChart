bfc6b9a1b92e6f6a04ab0f8c3c32bcab797f851b
sj0724 <sj07245@naver.com>
2025-03-29 11:49:38 +0900

    style: sonner 색상 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col items-center justify-center bg-gray-100`}
       >
-        <Toaster />
+        <Toaster richColors />
         <Providers>{children}</Providers>
       </body>
     </html>
